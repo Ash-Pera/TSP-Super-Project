e969eef438c38a7afc9c35d7fe0f3e9e4d97df0c
Ash Pera <aepera@mines.edu>
2019-09-03 10:45:19 -0600

    old and new exaustive search match, so deleted old one (including recursive enermeate all cycles)

--- The TSP Project/main.cpp
+++ The TSP Project/main.cpp
@@ -4,43 +4,20 @@
 #include <chrono> 
 
 
-void recursiveEnumerateAllCycles(Path startingPath, Graph& graph, std::vector<Cycle>& out) {
-	if (startingPath.numberOfPoints() == graph.getNumberPoints()) {
-		out.push_back(Cycle(startingPath));
-		return;
-	}
 
-	for (Point p : graph.getPointsByDistance(startingPath.getLastPoint())) {
-		if (!startingPath.containsPoint(p)) {
-			//might have to fix +
-			recursiveEnumerateAllCycles(startingPath + p, graph, out);
-		}
+Cycle shorterCycle(const Cycle c1, const Cycle c2) {
+	if (c1.length() < c2.length()) {
+		return c1;
+	}
+	else {
+		return c2;
 	}
 }
 
-std::vector<Cycle> enumerateAllCycles(Graph graph) {
-	auto out = std::vector<Cycle>();
-	//pick random starting point
-	Point start = graph.getNearestPoint(Point(0.5, 0.5));
-	Path startingPath = Path();
-	startingPath.addPoint(start);
-
-	recursiveEnumerateAllCycles(startingPath, graph, out);
-	return out;
+Cycle exaustiveSearch(Graph g) {
+	return g.reduceOverAllCycles(shorterCycle);
 }
 
-
-Cycle exaustiveSearch(Graph graph) {
-	std::vector<Cycle>  allCycles = enumerateAllCycles(graph);
-
-	Cycle minCycle = allCycles.front();
-	for (Cycle cycle : allCycles) {
-		if (cycle.length() < minCycle.length()) {
-			minCycle = cycle;
-		}
-	}
-	return minCycle;
-}
 
 using namespace std::chrono;
 void timeExaustiveSearch(int sizeOfGraph, int numberOfTimes) {
@@ -68,27 +45,12 @@ void timeGreedySearch(int sizeOfGraph, int numberOfTimes) {
 }
 
 
-Cycle shorterCycle(const Cycle c1, const Cycle c2) {
-	if (c1.length() < c2.length()) {
-		return c1;
-	}
-	else {
-		return c2;
-	}
-}
-
 
 int main(int argc, char *argv[]) { 
 	//std::cout.imbue(std::locale(""));
 
-	Graph graph = Graph(6);
 
-	Cycle oldBest = exaustiveSearch(graph);
-
-	Cycle newBest = graph.reduceOverAllCycles(shorterCycle);
-	std::cout << "old "<<oldBest << std::endl << std::endl;
-	
-	std::cout << newBest;
+	timeExaustiveSearch(11,2);
 
 	std::cin.get();
 
